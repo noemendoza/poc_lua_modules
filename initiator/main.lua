37fc5bfe750758a248b3f2147a9fba23458b1350
Noe Mendoza <noe.mendoza.g@gmail.com>
2023-04-14 17:15:09 -0600

    1

--- initiator/main.lua
+++ initiator/main.lua
@@ -2,9 +2,11 @@ local ngx = ngx
 
 local _M = {}
 
-
-
 function _M.rewrite()
-    ngx.log(ngx.NOTICE, "rewrite ------------------------") 
+  local ua = ngx.var.http_user_agent
+  ngx.log(ngx.NOTICE, "Hello") 
+  if ua == "hello" then
+    ngx.req.set_header("x-hello-world", "1")
+  end
 end
 
