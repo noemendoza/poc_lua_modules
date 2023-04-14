3d6b4d3cd3e142587c2fc0ebbf35f1f348c784df
Noe Mendoza <noe.mendoza.g@gmail.com>
2023-04-14 17:43:32 -0600

    006

--- initiator/main.lua
+++ initiator/main.lua
@@ -1,12 +1,2 @@
 local ngx = ngx
 
-local _M = {}
-
-function _M.rewrite()
-  local ua = ngx.var.http_user_agent
-  ngx_log(ngx.NOTICE, string_format("helll"))
-  if ua == "hello" then
-    ngx.req.set_header("x-hello-world", "1")
-  end
-end
-
