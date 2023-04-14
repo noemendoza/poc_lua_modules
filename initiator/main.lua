6c96154a476412f396e6fdd4937f39bc220b57b7
Noe Mendoza <noe.mendoza.g@gmail.com>
2023-04-14 17:13:42 -0600

    1

--- initiator/main.lua
+++ initiator/main.lua
@@ -2,14 +2,9 @@ local ngx = ngx
 
 local _M = {}
 
-function _M.init_worker()
-    ngx.log(ngx.NOTICE, "init_worker ------------------------") 
-end
+
 
 function _M.rewrite()
     ngx.log(ngx.NOTICE, "rewrite ------------------------") 
 end
 
-function _M.log()
-    ngx.log(ngx.NOTICE, "LOG  ------------------------")
-end
