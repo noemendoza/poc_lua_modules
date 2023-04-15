1e7812d9b86aad245637713f02a72dd2a42a7768
Noe Mendoza <noe.mendoza.g@gmail.com>
2023-04-14 18:20:23 -0600

    009

--- initiator/main.lua
+++ initiator/main.lua
@@ -7,10 +7,11 @@ local _M = {}
 -- end
 
 
+local prometheus = require("plugins/prometheus/main").new()
 
 function _M:init_worker()
     ngx.log(ngx.NOTICE, "init_worker =====================")
-    prometheus = require("plugins/prometheus/main").init()
+    prometheus.init("prometheus_metrics",{sync_interval=0.4})
     metric_requests = prometheus:counter("requests_total", "Number of HTTP requests", {"host", "status"})
     metric_latency = prometheus:histogram("request_duration_seconds","HTTP request latency", {"host"},{0.08, 0.089991, 0.1, 0.2, 0.75, 1, 1.5, 3.123232001, 5, 15, 120, 350.5, 1500, 75000, 1500000})
     metric_connections = prometheus:gauge("connections","Number of HTTP connections", {"state"})
