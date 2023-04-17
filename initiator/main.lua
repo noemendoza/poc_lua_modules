adf92689e224bce51f6f9e598d89641cbbf2968d
Noe Mendoza <noe.mendoza.g@gmail.com>
2023-04-17 11:57:07 -0600

    add rps metrics 4

--- initiator/main.lua
+++ initiator/main.lua
@@ -5,58 +5,26 @@ local _M = {}
 -- function _M.rewrite()
 --   ngx.log(ngx.NOTICE, "rewrite =====================")
 -- end
-
+local last_second = math.floor(ngx.now())
+local requests_this_second = 0
 
 
 function _M:init_worker()
     ngx.log(ngx.NOTICE, "init_worker =====================")
     prometheus = require("plugins/prometheus/main").init("prometheus_metrics",{sync_interval=0.4})
 
-    metric_requests = prometheus:counter("requests_total",
-    "Number of HTTP requests", {"host", "status"})
-    metric_latency = prometheus:histogram("request_duration_seconds",
-        "HTTP request latency", {"host"},
-        {0.08, 0.089991, 0.1, 0.2, 0.75, 1, 1.5, 3.123232001, 5, 15, 120, 350.5, 1500, 75000, 1500000})
-    metric_connections = prometheus:gauge("connections",
-        "Number of HTTP connections", {"state"})
-    metric_response_sizes = prometheus:histogram(
-        "nginx_http_response_size_bytes", "Size of HTTP responses", {"host"},
-        {10,100,1000,10000,100000,1000000})
-    metric_bytes = prometheus:counter("nginx_http_request_size_bytes",
-        "Total size of incoming requests", {"host"})
-    metric_requests_per_second = prometheus:gauge(
-        "requests_per_second_total", "Number of HTTP requests per second", {"host"})
-    metric_active_connections = prometheus:gauge(
-        "active_connections", "Number of active HTTP connections", {"host"})
-    metric_open_connections = prometheus:counter(
-        "open_connections_total", "Total number of HTTP connections opened", {"host"})
-    metric_closed_connections = prometheus:counter(
-        "closed_connections_total", "Total number of HTTP connections closed", {"host"})
-    
-    -- Agregar variables globales para mantener un seguimiento del recuento de solicitudes de las dos últimas segundas
-    last_second = 0
-    current_second = 0
-    request_count_last_second = 0
-    request_count_current_second = 0
-    
-    -- Use el temporizador de la biblioteca resty.stats para contar el número de solicitudes por segundo
-    local stats = require "resty.stats"
-    local last_time = ngx.now()
-    stats.start(1, function()
-        local now = ngx.now()
-        local delta = now - last_time
-        last_time = now
-        
-        -- Copiar el número actual de solicitudes al recuento del último segundo antes de actualizar el número actual
-        request_count_last_second = request_count_current_second
-        request_count_current_second = metric_requests:get()
-        
-        -- Calcular la tasa de solicitudes por segundo entre el recuento del último segundo y el recuento actual
-        local rate = (request_count_current_second - request_count_last_second) / delta
-        
-        -- Actualizar la métrica de tasa de solicitudes por segundo
-        metric_requests_per_second:set(rate, {domain})
-    end)
+    metric_requests = prometheus:counter("requests_total", "Number of HTTP requests", {"host", "status"})
+    metric_latency = prometheus:histogram("request_duration_seconds","HTTP request latency", {"host"},{0.08, 0.089991, 0.1, 0.2, 0.75, 1, 1.5, 3.123232001, 5, 15, 120, 350.5, 1500, 75000, 1500000})
+    metric_connections = prometheus:gauge("connections","Number of HTTP connections", {"state"})
+    metric_response_sizes = prometheus:histogram("response_sizes_bytes", "Size of HTTP responses", {"host"}, {10, 100, 1000, 10000, 100000, 1000000})
+    metric_bytes = prometheus:counter("request_sizes_bytes","Total size of incoming requests", {"host"})
+    metric_requests_per_second = prometheus:counter("requests_per_second_total", "Number of HTTP requests per second", {"host"})
+    metric_active_connections = prometheus:gauge("active_connections", "Number of active HTTP connections", {"host"})
+    metric_open_connections = prometheus:counter("open_connections_total", "Total number of HTTP connections opened", {"host"})
+    metric_closed_connections = prometheus:counter("closed_connections_total", "Total number of HTTP connections closed", {"host"})
+    -- Inicializar el temporizador
+
+
 end
 
 function _M:log()
@@ -64,6 +32,7 @@ function _M:log()
     local domain = string.match(host, "^([^%.]+)%.") or "unknown"
     metric_requests:inc(1, {domain, ngx.var.status})
     metric_latency:observe(tonumber(ngx.var.request_time), {domain})
+    metric_response_sizes:observe(tonumber(ngx.var.bytes_sent), {domain})
     metric_bytes:inc(tonumber(ngx.var.request_length), {domain})
     metric_requests_per_second:inc(1, {domain})
 
