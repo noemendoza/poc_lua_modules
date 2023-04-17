a11d8e85a37c60c26a6947eef45fad62bac677b0
Noe Mendoza <noe.mendoza.g@gmail.com>
2023-04-17 13:48:06 -0600

    add rps metrics 9

--- initiator/main.lua
+++ initiator/main.lua
@@ -11,27 +11,21 @@ local _M = {}
 function _M:init_worker()
     ngx.log(ngx.NOTICE, "init_worker =====================")
     prometheus = require("plugins/prometheus/main").init("prometheus_metrics",{sync_interval=0.4})
+
     metric_requests = prometheus:counter("requests_total",
-        "Number of HTTP requests", {"host", "status"})
+    "Number of HTTP requests", {"host", "status"})
     metric_latency = prometheus:histogram("request_duration_seconds",
         "HTTP request latency", {"host"},
         {0.08, 0.089991, 0.1, 0.2, 0.75, 1, 1.5, 3.123232001, 5, 15, 120, 350.5, 1500, 75000, 1500000})
     metric_connections = prometheus:gauge("connections",
         "Number of HTTP connections", {"state"})
-    metric_response_sizes = prometheus:histogram(
-        "nginx_http_response_size_bytes", "Size of HTTP responses", {"host"},
-        {10,100,1000,10000,100000,1000000})
-    metric_bytes = prometheus:counter("nginx_http_request_size_bytes",
-        "Total size of incoming requests", {"host"})
-    metric_requests_per_second = prometheus:counter(
-        "requests_per_second_total", "Number of HTTP requests per second", {"host"})
-    metric_active_connections = prometheus:gauge(
-        "active_connections", "Number of active HTTP connections", {"host"})
-    metric_open_connections = prometheus:counter(
-        "open_connections_total", "Total number of HTTP connections opened", {"host"})
-    metric_closed_connections = prometheus:counter(
-        "closed_connections_total", "Total number of HTTP connections closed", {"host"})
-
+    metric_response_sizes = prometheus:histogram("response_sizes",
+        "Size of HTTP responses", {"host"},
+        {100, 200, 500, 1000, 1500, 2000, 3000, 5000, 7500, 10000, 20000, 50000, 75000, 100000, 200000, 500000})
+    metric_bytes = prometheus:counter("bytes_total",
+        "Number of bytes sent and received", {"host", "direction"})
+    metric_requests_per_second = prometheus:counter("requests_per_second",
+        "Number of HTTP requests per second", {"host"})
 end
 
 function _M:log()
@@ -39,9 +33,10 @@ function _M:log()
     local domain = string.match(host, "^([^%.]+)%.") or "unknown"
     metric_requests:inc(1, {domain, ngx.var.status})
     metric_latency:observe(tonumber(ngx.var.request_time), {domain})
-    metric_bytes:inc(tonumber(ngx.var.request_length), {domain})
+    metric_response_sizes:observe(tonumber(ngx.var.bytes_sent), {domain})
+    metric_bytes:inc(tonumber(ngx.var.bytes_sent), {domain, "sent"})
+    metric_bytes:inc(tonumber(ngx.var.bytes_received), {domain, "received"})
     metric_requests_per_second:inc(1, {domain})
-
 end
 
 return _M
