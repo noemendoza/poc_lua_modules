9ec6f159847347d4045cb1cabe0e1db49c45f49d
Noe Mendoza <noe.mendoza.g@gmail.com>
2023-04-17 10:33:21 -0600

    aupdate lua moduleggp

--- initiator/main.lua
+++ initiator/main.lua
@@ -11,9 +11,13 @@ local _M = {}
 function _M:init_worker()
     ngx.log(ngx.NOTICE, "init_worker =====================")
     prometheus = require("plugins/prometheus/main").init("prometheus_metrics",{sync_interval=0.4})
+    metric_bytes = prometheus:counter("nginx_http_request_size_bytes", "Total size of incoming requests")
     metric_requests = prometheus:counter("requests_total", "Number of HTTP requests", {"host", "status"})
     metric_latency = prometheus:histogram("request_duration_seconds","HTTP request latency", {"host"},{0.08, 0.089991, 0.1, 0.2, 0.75, 1, 1.5, 3.123232001, 5, 15, 120, 350.5, 1500, 75000, 1500000})
     metric_connections = prometheus:gauge("connections","Number of HTTP connections", {"state"})
+    metric_response_sizes = prometheus:histogram(
+        "nginx_http_response_size_bytes", "Size of HTTP responses", nil,
+        {10,100,1000,10000,100000,1000000})
 end
 
 function _M:log()
